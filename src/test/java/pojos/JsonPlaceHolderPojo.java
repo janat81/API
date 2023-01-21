34045c11c4d9cee14da5e1973e62d8f55386d51a
zhanat güneş <guneszhanat@gmail.com>
2023-01-21 10:03:47 +0300

    go

---
+++ src/test/java/pojos/JsonPlaceHolderPojo.java
@@ -0,0 +1,65 @@
+package pojos;
+
+
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class JsonPlaceHolderPojo {
+    /*
+    1) Create private variables for all keys
+    2) Create constructors with all parameters and without any parameter.
+    3) Create public getter and setter methods
+    4) Create toString() method
+     */
+    private Integer userId;
+    private String title;
+    private Boolean completed;
+
+    public JsonPlaceHolderPojo(Integer userId, String title, Boolean completed) {
+        this.userId = userId;
+        this.title = title;
+        this.completed = completed;
+    }
+
+    public JsonPlaceHolderPojo() {
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+
+    @Override
+    public String toString() {
+        return "JsonPlaceHolderPojo{" +
+                "userId=" + userId +
+                ", title='" + title + '\'' +
+                ", completed=" + completed +
+                '}';
+    }
+
+    /*
+    How do we handle different key-values in response?
+    We use @JsonIgnoreProperties(ignoreUnknown = true) annotation at the top of the Pojo class.
+    It comes from "org.codehaus.jackson.annotate.JsonIgnoreProperties"
+     */
+}
